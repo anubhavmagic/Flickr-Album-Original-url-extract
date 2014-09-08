18196bc32b6f29af03124aed3485ff5d91498e2f
Anubhav Bimbisariye <i-anubhav.bimbisariye@semusi.com>
2014-09-09 03:20:41 +0530

    pic extracted testing remaining

---
+++ fl.py
@@ -0,0 +1,24 @@
+import re
+import urllib
+from pprint import pprint
+import json
+
+firstpic ='https://www.flickr.com/photos/126226072@N03/14811500040/in/set-72157646731999905'
+#---------- Open file ----------
+opened = urllib.urlopen(firstpic)
+code = opened.read()
+f = open('urls.txt','w')
+# f.write(code)
+
+#---------- Open file ----------
+
+picurl = r'(Y\.photo\.init\()(.+)\);'
+matched = re.search(picurl,code)
+if matched:
+  converted = matched.group(2)
+  data=json.loads(converted)
+  data=data[u'sizes'][u'o'][u'url']
+  f.write(data)
+else:
+	print 'not found'
+f.close()
